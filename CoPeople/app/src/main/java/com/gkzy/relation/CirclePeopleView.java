ccdef71115775044472b95b4d67f116f0a16e586
Harry Chen <564849121@qq.com>
2018-08-30 15:29:29 +0800

    修改了连线超过圆的问题，修改了随机长度为8的问题

--- CoPeople/app/src/main/java/com/gkzy/relation/CirclePeopleView.java
+++ CoPeople/app/src/main/java/com/gkzy/relation/CirclePeopleView.java
@@ -11,7 +11,6 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -31,8 +30,9 @@ public class CirclePeopleView extends View {
     private Paint paint;
     private float XPoint;
     private float YPoint;
-    private int[] radius = new int[8];
+    private int[] radius = null;
     private List<CoPersonBean> lists = new ArrayList<>();
+    private static final int DISTANCE_CIRCLE = 180;
 
     public CirclePeopleView(Context context) {
         super(context);
@@ -48,9 +48,10 @@ public class CirclePeopleView extends View {
 
     public void SetFieldInfo(List<CoPersonBean> lists) {
         this.lists = lists;
-        for (int i = 0; i < 8; i++) {
+        radius = new int[lists.size()];
+        for (int i = 0; i < lists.size(); i++) {
             Random r = new Random();
-            radius[i] = r.nextInt(160) + 180;
+            radius[i] = r.nextInt(160) + DISTANCE_CIRCLE;
         }
     }
 
@@ -66,11 +67,11 @@ public class CirclePeopleView extends View {
         this.paint.setStrokeWidth(dip2px(mContext, 2));
         XPoint = getWidth() / 2;
         YPoint = getHeight() / 2;
-        canvas.drawCircle(XPoint, YPoint, dip2px(mContext, 20), paint);
+        canvas.drawCircle(XPoint, YPoint, dip2px(mContext, 22), paint);
 
         this.paint.setColor(Color.parseColor("#333333"));
         this.paint.setTextSize(dip2px(mContext, 10));
-        canvas.drawText(MyApplication.childSub, XPoint - 40, YPoint + 60, paint);
+        canvas.drawText(MyApplication.childSub, XPoint - 40, YPoint + dip2px(mContext, 32), paint);
         int count = lists.size();
         if (lists != null && count > 0) {
             double pere = Math.PI * 2 / count;
@@ -78,16 +79,19 @@ public class CirclePeopleView extends View {
                 paint.setColor(Color.parseColor(colors[i % colors.length]));
                 paint.setStrokeWidth(dip2px(mContext, 2));
                 int r = lists.get(i).getSymbolSize();
-                if (r < 8){
+                if (r < 8) {
                     r = 8;
+                } else if (r > 22) {
+                    r = 22;
                 }
-                canvas.drawCircle(XPoint + (float) Math.cos(pere * i + 5) * radius[i % radius.length], YPoint + (float) Math.sin(pere * i + 5) * radius[i % radius.length], dip2px(mContext, r), paint);
+                canvas.drawCircle(XPoint + (float) Math.cos(pere * i + 5) * radius[i], YPoint + (float) Math.sin(pere * i + 5) * radius[i], dip2px(mContext, r), paint);
                 this.paint.setColor(Color.parseColor("#333333"));
                 this.paint.setTextSize(dip2px(mContext, 8));
-                canvas.drawText(lists.get(i).getName(), XPoint + (float) Math.cos(pere * i + 5) * radius[i % radius.length] - 30, YPoint + (float) Math.sin(pere * i + 5) * radius[i % radius.length] + 35, paint);
+                this.paint.setTextAlign(Paint.Align.LEFT);
+                canvas.drawText(lists.get(i).getName(), XPoint + (float) Math.cos(pere * i + 5) * radius[i] - 30, YPoint + (float) Math.sin(pere * i + 5) * radius[i] + dip2px(mContext, r + 10), paint);
                 this.paint.setColor(Color.parseColor("#666666"));
                 this.paint.setStrokeWidth(0.5f);
-                canvas.drawLine(XPoint + (float) Math.cos(pere * i + 5) * 40, YPoint + (float) Math.sin(pere * i + 5) * 40, XPoint + (float) Math.cos(pere * i + 5) * (radius[i % radius.length] - 20), YPoint + (float) Math.sin(pere * i + 5) * (radius[i % radius.length] - 20), paint);
+                canvas.drawLine(XPoint + (float) Math.cos(pere * i + 5) * dip2px(mContext, 22), YPoint + (float) Math.sin(pere * i + 5) * dip2px(mContext, 22), XPoint + (float) Math.cos(pere * i + 5) * (radius[i] - dip2px(mContext, r)), YPoint + (float) Math.sin(pere * i + 5) * (radius[i] - dip2px(mContext, r)), paint);
 
             }
         }
@@ -98,5 +102,6 @@ public class CirclePeopleView extends View {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale);
     }
+
 
 }
